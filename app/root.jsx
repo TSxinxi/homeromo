f09f32cc53775c0f60c255f890f715cb49f2ca55
enxiu <hd1783260996@163.com>
2023-11-01 18:18:18 +0800

    时间截取前十位、监控报错隐藏、隐藏填写评论

--- app/root.jsx
+++ app/root.jsx
@@ -74,21 +74,21 @@ export async function loader({ context }) {
 }
 
 export default function App() {
-  Sentry.init({
-    dsn: "https://3a9a729470c367d1557919b68842f912@o4506097284677632.ingest.sentry.io/4506098198773760",
-    integrations: [
-      new Sentry.BrowserTracing({
-        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: ["localhost", /^https:\/\/page.homeromo.com\.io\/api/],
-      }),
-      new Sentry.Replay(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, // Capture 100% of the transactions
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  });
+  // Sentry.init({
+  //   dsn: "https://3a9a729470c367d1557919b68842f912@o4506097284677632.ingest.sentry.io/4506098198773760",
+  //   integrations: [
+  //     new Sentry.BrowserTracing({
+  //       // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  //       tracePropagationTargets: ["localhost", /^https:\/\/page.homeromo.com\.io\/api/],
+  //     }),
+  //     new Sentry.Replay(),
+  //   ],
+  //   // Performance Monitoring
+  //   tracesSampleRate: 1.0, // Capture 100% of the transactions
+  //   // Session Replay
+  //   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  //   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  // });
 
   const data = useLoaderData();
   const locale = data.selectedLocale ?? DEFAULT_LOCALE;
