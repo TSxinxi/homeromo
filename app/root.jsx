65b17b16be9618af13c322dc9491a1455595948c
enxiu <hd1783260996@163.com>
2023-11-02 11:14:32 +0800

    报警初始化

--- app/root.jsx
+++ app/root.jsx
@@ -74,21 +74,6 @@ export async function loader({ context }) {
 }
 
 export default function App() {
-  // Sentry.init({
-  //   dsn: "https://3a9a729470c367d1557919b68842f912@o4506097284677632.ingest.sentry.io/4506098198773760",
-  //   integrations: [
-  //     new Sentry.BrowserTracing({
-  //       // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  //       tracePropagationTargets: ["localhost", /^https:\/\/page.homeromo.com\.io\/api/],
-  //     }),
-  //     new Sentry.Replay(),
-  //   ],
-  //   // Performance Monitoring
-  //   tracesSampleRate: 1.0, // Capture 100% of the transactions
-  //   // Session Replay
-  //   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  //   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  // });
 
   const data = useLoaderData();
   const locale = data.selectedLocale ?? DEFAULT_LOCALE;
@@ -102,6 +87,22 @@ export default function App() {
       localStorage.setItem('refererName', getReferer())
     }
     useEffect(() => {
+      Sentry.init({
+        dsn: "https://3a9a729470c367d1557919b68842f912@o4506097284677632.ingest.sentry.io/4506098198773760",
+        integrations: [
+          new Sentry.BrowserTracing({
+            // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+            tracePropagationTargets: ["localhost", /^https:\/\/page.homeromo.com\.io\/api/],
+          }),
+          new Sentry.Replay(),
+        ],
+        // Performance Monitoring
+        tracesSampleRate: 1.0, // Capture 100% of the transactions
+        // Session Replay
+        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+      });
+
       (function (h, o, t, j, a, r) {
         h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
         h._hjSettings = { hjid: 3612403, hjsv: 6 };
